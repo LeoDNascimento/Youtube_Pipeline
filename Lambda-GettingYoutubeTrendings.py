b7f55e97e8cfdda744dc92baea8c06bba2b0d3ed
LeoDNascimento <leonardo.nascimento1306@gmail.com>
2024-02-13 16:56:44 -0300

    Feat: funçãoo pegar dados do youtube API

--- Lambda-GettingYoutubeTrendings.py
+++ Lambda-GettingYoutubeTrendings.py
@@ -10,19 +10,18 @@ load_dotenv()
 
 # Defina suas credenciais da API do YouTube
 DEVELOPER_KEY = os.getenv("YOUTUBE_API_KEY")
-"AIzaSyA34Y_jgiRwdQrZo_zyMNjKsrPBDc5vtis"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 # Defina o país para o qual você deseja buscar os vídeos em tendência (BR para Brasil)
 REGION_CODE = "BR"
 
-# def normalize_text(text):
-#     """
-#     Função para normalizar texto removendo diacríticos e convertendo para UTF-8.
-#     """
-#     normalized_text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8')
-#     return normalized_text
+def normalize_text(text):
+    """
+    Função para normalizar texto removendo diacríticos e convertendo para UTF-8.
+    """
+    normalized_text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8')
+    return normalized_text
 
 def get_trending_videos():
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
